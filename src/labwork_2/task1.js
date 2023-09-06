cfe1348dc669fd29558e9c3fb401a1b6b95772c2
Delnegend <kiennguyen19323@gmail.com>
2023-09-06 20:05:58 +0700

    refactor(labwork 2): :recycle: centering, remove head/html tags

--- src/labwork_2/task1.js
+++ src/labwork_2/task1.js
@@ -1,11 +1,9 @@
 function CelToFah(celsius) {
-    var fahrenheit = (celsius * 9 / 5) + 32
-    console.log(celsius + " degree Celsius is equal to " + fahrenheit + " degree Fahrenheit.")
+    var fahrenheit = (celsius * 9) / 5 + 32;
+    console.log(celsius + " degree Celsius is equal to " + fahrenheit + " degree Fahrenheit.");
 }
 
 function FahToCel(fahrenheit) {
-    var celsius = (fahrenheit - 32) * 5 / 9
-    console.log(fahrenheit + " degree Fahrenheit is equal to " + celsius + " degree Celsius.")
+    var celsius = ((fahrenheit - 32) * 5) / 9;
+    console.log(fahrenheit + " degree Fahrenheit is equal to " + celsius + " degree Celsius.");
 }
-
-
--- src/labwork_2/task3.js
+++ src/labwork_2/task3.js
@@ -1,10 +1,9 @@
-
 function oddEven() {
     for (let i = 1; i <= 15; i++) {
         if (i % 2 === 0) {
-            console.log(i + ' is even');
+            console.log(i + " is even");
         } else {
-            console.log(i + ' is odd');
+            console.log(i + " is odd");
         }
     }
 }
