d24b2392597dd5fc74d5da74e1fd9ebb29b240f9
EllimacH <92161394+EllimacH@users.noreply.github.com>
2023-09-06 13:55:03 +0700

    finish task 1 4 7

---
+++ src/labwork_2/task1.js
@@ -0,0 +1,11 @@
+function CelToFah(celsius) {
+    var fahrenheit = (celsius * 9 / 5) + 32
+    console.log(celsius + " degree Celsius is equal to " + fahrenheit + " degree Fahrenheit.")
+}
+
+function FahToCel(fahrenheit) {
+    var celsius = (fahrenheit - 32) * 5 / 9
+    console.log(fahrenheit + " degree Fahrenheit is equal to " + celsius + " degree Celsius.")
+}
+
+
