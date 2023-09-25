b9d49c6078a0ceef0c88e52f56cb45eca0418f7d
Dao Tuyet Ngan (Peachy) <107946882+Peachy72@users.noreply.github.com>
2023-09-25 20:58:09 +0700

    feat(task3.php): labwork 3 - Task 3

---
+++ src/labwork_3/task3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task 3</title>
+</head>
+<body>
+    <?php
+        echo "<table border=\"1\" width=\"500\" align=\"center\">";
+        echo "<tr align=\"center\"><td><b>Firstname</b></td><td><b>Lastname</b></td><td><b>Age</b></td></tr>";
+        echo "<tr><td>Jill</td><td>Smith</td><td>50</td></tr>";
+        echo "<tr><td>Eve</td><td>Jackson</td><td>94</td></tr>";
+        echo "<tr><td>John</td><td>Doe</td><td>80</td></tr>";
+        echo "</table>";
+    ?>
+</body>
+</html>
